b7a04ae5a1037b565f3da947e95e85188e9b5307
Harold <sadcah@yahoo.com>
2023-09-07 23:15:07 +0100

    feat:changed X button CSS

--- client/src/components/ProfileSection.jsx
+++ client/src/components/ProfileSection.jsx
@@ -1,24 +1,52 @@
-import { useSelector } from "react-redux"
+import { useSelector } from 'react-redux';
 
+function ProfileSection({
+  removeRecipe,
+  openRecipe,
+  typemessage,
+  type,
+  recipe,
+}) {
+  const currentUser = useSelector((state) => state.currentUser);
 
-function ProfileSection({removeRecipe, openRecipe, typemessage, type, recipe}){
-  const currentUser = useSelector(state => state.currentUser)
-
-  return(
-    <div className="user-food-favourites">
-    <span id="recipe-label">{typemessage} Foods:</span>
-    <span className="content" >
-      {currentUser[type].length ? (
-            currentUser[type].map((el) => {
-              return <span className="fav-name-con">
-                <span className="fav-name" key={el._id} id={el._id} onClick={(e) => {openRecipe(e, recipe)}}>{el.name}</span>
-                <button id={el._id} className="remove-fav" onClick={(e)=> {removeRecipe(e, recipe, type)}}>X</button>
+  return (
+    <div className='user-food-favourites'>
+      <span id='recipe-label'>{typemessage} Foods:</span>
+      <span className='content'>
+        {currentUser[type].length ? (
+          currentUser[type].map((el) => {
+            return (
+              <span className='fav-name-con'>
+                <span
+                  className='fav-name'
+                  key={el._id}
+                  id={el._id}
+                  onClick={(e) => {
+                    openRecipe(e, recipe);
+                  }}
+                >
+                  {el.name}
                 </span>
-            })
-          ) : <span className="meal">You dont have any {typemessage} recipe yet.</span>}
-    </span>
+                <button
+                  id={el._id}
+                  className='btn-plus'
+                  onClick={(e) => {
+                    removeRecipe(e, recipe, type);
+                  }}
+                >
+                  X
+                </button>
+              </span>
+            );
+          })
+        ) : (
+          <span className='meal'>
+            You dont have any {typemessage} recipe yet.
+          </span>
+        )}
+      </span>
     </div>
-  )
+  );
 }
 
-export default ProfileSection
+export default ProfileSection;
