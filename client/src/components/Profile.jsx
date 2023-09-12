13ec3c7f9f1fdafb1f9c61eb96bab5703a9c4e86
Harold <sadcah@yahoo.com>
2023-09-12 10:02:50 +0100

    feat:changed profile layout

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,72 +1,111 @@
-import { setDrink} from "../redux/actions"
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import Loader from "./Loader";
-import services from "./Services";
-import ProfileSection from "./ProfileSection";
-import '../css/profile.css'
+import { setDrink } from '../redux/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Loader from './Loader';
+import services from './Services';
+import ProfileSection from './ProfileSection';
+import '../css/profile.css';
 
 function Profile() {
-  const authenticated = useSelector(state => state.authenticated)
-  const currentUser = useSelector(state => state.currentUser)
-  const loadPage = useSelector(state => state.loadPage)
+  const authenticated = useSelector((state) => state.authenticated);
+  const currentUser = useSelector((state) => state.currentUser);
+  const loadPage = useSelector((state) => state.loadPage);
   const navigate = useNavigate();
-  const dispatch = useDispatch()
-
+  const dispatch = useDispatch();
 
   function openRecipe(e, recipeType) {
     const id = e.target.id;
-    if(recipeType == 'drink') {
-      const userDrink = currentUser.drinkFavourites.filter((el) => el._id == id)
-      dispatch(setDrink(...userDrink))
-    } else if(recipeType == 'recipe') {
-      const userFood = currentUser.personalRecipes.filter((el) => el._id == id)
+    if (recipeType == 'drink') {
+      const userDrink = currentUser.drinkFavourites.filter(
+        (el) => el._id == id
+      );
+      dispatch(setDrink(...userDrink));
+    } else if (recipeType == 'recipe') {
+      const userFood = currentUser.personalRecipes.filter((el) => el._id == id);
     }
-    navigate(`/${recipeType}/${id}`)
+    navigate(`/${recipeType}/${id}`);
   }
-  console.log(useSelector(state => state.currentUser))
+  console.log(useSelector((state) => state.currentUser));
 
-  function removeRecipe (e, recipeType, path) {
+  function removeRecipe(e, recipeType, path) {
     const _id = e.target.id;
-    const filtered = currentUser[path].filter((el) => el._id != _id)
-    services.removeFavourite({_id}, path).then((res) => {
-      currentUser[path] = filtered
-      navigate(`/user-profile`)
-    })
+    const filtered = currentUser[path].filter((el) => el._id != _id);
+    services.removeFavourite({ _id }, path).then((res) => {
+      currentUser[path] = filtered;
+      navigate(`/user-profile`);
+    });
   }
-  if(!loadPage) {
-    return (
-      < Loader />
-    )
+  if (!loadPage) {
+    return <Loader />;
   } else if (loadPage) {
     return (
-      <div key={'profile'} className="profile-container">{authenticated ? (
+      <div key={'profile'} className='profile-container'>
+        {authenticated ? (
           <>
-          <span key={currentUser.username} className="profile-label">{currentUser.username}</span>
-          <div key={currentUser.username} className="user-profile">
-            <div className="user-recipes">
-            <div className="day-plan">
-                <ProfileSection removeRecipe={removeRecipe} openRecipe={openRecipe} typemessage={'breakfast'} type={'breakfast'} recipe={'recipe'} />
-                <ProfileSection removeRecipe={removeRecipe} openRecipe={openRecipe} typemessage={'lunch'} type={'lunch'} recipe={'recipe'} />
-                <ProfileSection removeRecipe={removeRecipe} openRecipe={openRecipe} typemessage={'dinner'} type={'dinner'} recipe={'recipe'} />
-              </div>
-              <div className="fave">
-                <ProfileSection removeRecipe={removeRecipe} openRecipe={openRecipe} typemessage={'favourite'} type={'foodFavourites'} recipe={'recipe'} />
-                <ProfileSection removeRecipe={removeRecipe} openRecipe={openRecipe} typemessage={'personal'} type={'personalRecipes'} recipe={'recipe'} />
-                <ProfileSection removeRecipe={removeRecipe} openRecipe={openRecipe} typemessage={'drink'} type={'drinkFavourites'} recipe={'drink'} />
+            <span key={currentUser.username} className='profile-label'>
+              {currentUser.username}
+            </span>
+            <div key={currentUser.username} className='user-profile'>
+              <div className='user-recipes'>
+                <div className='day-plan'>
+                  <ProfileSection
+                    removeRecipe={removeRecipe}
+                    openRecipe={openRecipe}
+                    typemessage={'breakfast'}
+                    type={'breakfast'}
+                    recipe={'recipe'}
+                  />
+                  <ProfileSection
+                    removeRecipe={removeRecipe}
+                    openRecipe={openRecipe}
+                    typemessage={'lunch'}
+                    type={'lunch'}
+                    recipe={'recipe'}
+                  />
+                  <ProfileSection
+                    removeRecipe={removeRecipe}
+                    openRecipe={openRecipe}
+                    typemessage={'dinner'}
+                    type={'dinner'}
+                    recipe={'recipe'}
+                  />
+                </div>
+                <div className='fave'>
+                  <ProfileSection
+                    removeRecipe={removeRecipe}
+                    openRecipe={openRecipe}
+                    typemessage={'favourite'}
+                    type={'foodFavourites'}
+                    recipe={'recipe'}
+                  />
+                  <ProfileSection
+                    removeRecipe={removeRecipe}
+                    openRecipe={openRecipe}
+                    typemessage={'personal'}
+                    type={'personalRecipes'}
+                    recipe={'recipe'}
+                  />
+                  <ProfileSection
+                    removeRecipe={removeRecipe}
+                    openRecipe={openRecipe}
+                    typemessage={'drink'}
+                    type={'drinkFavourites'}
+                    recipe={'drink'}
+                  />
+                </div>
               </div>
             </div>
-          </div>
           </>
-      ): (
-        <>
-          <span>COME ON MAN YOU CANNOT HAVE A PROFILE WITHOUT LOGGING IN</span>
-        </>
-      )
-        }
+        ) : (
+          <>
+            <span>
+              COME ON MAN YOU CANNOT HAVE A PROFILE WITHOUT LOGGING IN
+            </span>
+          </>
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default Profile
+export default Profile;
